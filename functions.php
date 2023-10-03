00354d0e742866b48eeb4cc4d82a4667c5f552da
Meriem <mekrarbech12@gmail.com>
2023-10-03 14:11:55 -0400

    init

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+function nameLengthIsValid($nameToValid){
+    $length=strlen($nameToValid);
+    $responses=[
+        'isValid'=>true,
+        'msg'=>''
+    ];
+    if($length<2){
+        $responses=[
+            'isValid'=>false,
+            'msg'=>'Nom trop court'
+        ];
+    }
+    elseif($length>10){
+        $responses=[
+            'isValid'=>false,
+            'msg'=>'Nom trop long'
+        ];
+    }
+    return $responses;
+}
+?>
---
+++ result.php
@@ -0,0 +1,16 @@
+<?php
+    require_once("functions.php");
+    var_dump($_POST);
+    if($_POST){
+        $name=$_POST['fname'];
+        if(empty($name)){
+            echo "</br> Nom vide";
+        }else{
+            echo "</br> Mon nom est :" . $name;
+        }
+    $nameLengthIsValid = nameLengthIsValid($_POST['fname']);
+    echo '</br>';
+    var_dump($nameLengthIsValid);
+    if(!$nameLengthIsValid['isValid']);
+    }
+?>
